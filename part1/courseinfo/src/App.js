f9712c4a670b3657ee1e79f0c6573943c4f1c072
log101 <frknerdm@mail.com>
2022-04-07 05:43:12 +0300

    refactor course info

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,45 +1,41 @@
 
-const Header = (props) => <h1>{props.header}</h1>
+const Header = (props) => <h1>{props.content.name}</h1>
 
-const Content = (props) => props.content.map(
+const Content = (props) => props.content.parts.map(
   (item) => <Part key={item.id} item={item} />
 )
 
-const Part = (props) => <p>{props.item.part} {props.item.exercise}</p>
+const Part = (props) => <p>{props.item.name} {props.item.exercises}</p>
 
-const Total = (props) => props.content.length
+const Total = (props) => props.content.parts.length
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        id: 1,
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        id: 2,
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        id: 3,
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
-  const contents = [
-    {
-      id: 1,
-      part: part1,
-      exercise: exercises1
-    },
-    {
-      id: 2,
-      part: part2,
-      exercise: exercises2
-    },
-    {
-      id: 3,
-      part: part3,
-      exercise: exercises3
-    }
-  ]
   return (
     <div>
-      <Header header={course} />
-      <Content content={contents} />
-      <Total content={contents} />      
+      <Header content={course} />
+      <Content content={course} />
+      <Total content={course} />      
     </div>
   )
 }
